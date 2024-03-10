79bf997b3b9751fb85851614b4ee123283f0b095
yizhenpong <yizhenpong@gmail.com>
2024-03-11 00:42:08 +0800

    clean dataset interface ready, tested

--- src/SpeakSingapore/create_data/clean_dataset.py
+++ src/SpeakSingapore/create_data/clean_dataset.py
@@ -1,14 +1,16 @@
 '''clean dataset module'''
 import pathlib
+import re
 
 class AbstractCleanDataset:
     '''Abstract class'''
-    def __init__(self, name: str, SFP: list, data_path: pathlib.Path, clean_data_path: pathlib.Path) -> None:
+    def __init__(self, name: str, SFP: list, data_path: pathlib.Path, clean_data_path: pathlib.Path, source: str) -> None:
         '''initialise with dataset name and a list of sentence final particles to be identified'''
         self.name = name
         self.SFP = SFP
         self.file_path = data_path
         self.clean_data_path = clean_data_path
+        self.source = source
     
     def singlish_SFP_present(self, sentence: str) -> bool:
         '''Check if Singlish Sentence Final Particle is present in sentence'''
@@ -26,19 +28,28 @@ class CleanDataset(AbstractCleanDataset):
     '''Clean dataset -- generic'''
     def singlish_SFP_present(self, sentence: str) -> bool:
         '''check if singlish SFP in sentence'''
+        punctuation_pattern = r'[^\w\s]'
+        cleaned_sentence = re.sub(punctuation_pattern, '', sentence)
         sfp_set = set(self.SFP)
-        for word in sentence.split():
+        for word in cleaned_sentence.split():
             if word.lower() in sfp_set:
                 return True
         return False
     
     def put_singlish_particle_within_delimiter(self, sentence: str, start_delimiter: str, end_delimiter: str) -> str:
         '''Place Singlish words within specified delimiters'''
-        if self.singlish_SFP_present(sentence):
-            words = sentence.split()
+        punctuation_pattern = r'[^\w\s]'
+        cleaned_sentence = re.sub(punctuation_pattern, '', sentence)
+        if self.singlish_SFP_present(cleaned_sentence):
+            words = cleaned_sentence.split()
             for i, word in enumerate(words):
                 if word.lower() in self.SFP:
                     words[i] = f"{start_delimiter}{word}{end_delimiter}"
             return ' '.join(words)
         else:
-            return sentence
+            raise Exception("Unexpected error when placing delimiters in sentence")
+    
+    def save_sentence_to_file(self, sentence: str):
+        '''Save sentence to file'''
+        with open(self.clean_data_path, 'a') as file:
+            file.write(sentence + '\n')
--- tests/create_data/clean_dataset_test.py
+++ tests/create_data/clean_dataset_test.py
@@ -4,14 +4,29 @@ sys.path.append(r"/Users/pongyizhen/Desktop/Github/SpeakSingapore/src")
 from SpeakSingapore.create_data.clean_dataset import CleanDataset
 
 def singlish_SFP_present_test():
-    clean_dataset = CleanDataset("Test Dataset", ["lah", "leh", "lor"], "bin/llm_data.json", "bin/testing.txt")
+    clean_dataset = CleanDataset("Test Dataset", ["lah", "leh", "lor"], "recycle_bin/llm_data.json", "recycle_bin/testing.txt", "temporary source")
     # Test case with Singlish SFP present
-    sentence_with_SFP = "This is a test sentence with lah."
+    sentence_with_SFP = "This is a test sentence with lah"
     assert clean_dataset.singlish_SFP_present(sentence_with_SFP) == True
+    sentence_with_SFP_punc = "This is a test sentence with lah."
+    assert clean_dataset.singlish_SFP_present(sentence_with_SFP_punc) == True
     # Test case without any Singlish SFP
     sentence_without_SFP = "This is a test sentence without any Singlish SFP."
     assert clean_dataset.singlish_SFP_present(sentence_without_SFP) == False
 
+def put_singlish_particle_within_delimiter_test():
+    clean_dataset = CleanDataset("Test Dataset", ["lah", "leh", "lor"], "recycle_bin/llm_data.json", "recycle_bin/testing.txt", "temporary source")
+    sentence_with_SFP = "This is a test sentence with lah"
+    assert clean_dataset.put_singlish_particle_within_delimiter(sentence_with_SFP, "[", "]") == "This is a test sentence with [lah]"
+
+def save_sentence_to_file_test():
+    clean_dataset = CleanDataset("Test Dataset", ["lah", "leh", "lor"], "recycle_bin/llm_data.json", "recycle_bin/testing.txt", "temporary source")
+    sentence_with_SFP = "This is a test sentence with lah"
+    clean_dataset.save_sentence_to_file(sentence_with_SFP)
+
 
 if __name__ == '__main__':
     singlish_SFP_present_test()
+    put_singlish_particle_within_delimiter_test()
+    save_sentence_to_file_test()
+    save_sentence_to_file_test()
