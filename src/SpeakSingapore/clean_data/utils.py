2660c2541e0d1173faf0c1f9b58e71d25fab6d76
yizhenpong <yizhenpong@gmail.com>
2024-03-21 22:20:45 +0800

    delete files

--- src/SpeakSingapore/clean_data/utils.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def write_to_json_file(file_path, object, string = False):
-    if string:
-        object = json.loads(object)
-    json_object = json.dumps(object, indent=4)
-    with open(file_path, "w") as file:
-        file.write(json_object)
-    print(f"contents saved to {file_path}")
